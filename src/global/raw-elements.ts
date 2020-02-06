2f7358a561892528e175672da261394b355675c1
Cary Smith <csmith01@up.com>
2020-02-06 15:11:55 -0600

    use templating

--- src/global/raw-elements.ts
+++ src/global/raw-elements.ts
@@ -6,30 +6,40 @@ const sheet = `
     position: relative;
     vertical-align: top;
 }`;
+const config: ShadowRootInit = { mode: "open" };
+
+const simpleTpl = document.createElement("template");
+simpleTpl.innerHTML = `
+  <style>${sheet}</style><span>Raw Native</span>
+`;
+
 class SimpleRaw extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = this.template;
-  }
-
-  get template() {
-    return `<style>${sheet}</style>
-            <span>Raw Native</span>`;
+    this.attachShadow(config);
+    this.shadowRoot.appendChild(simpleTpl.content.cloneNode(true));
   }
 }
+
+const nestedTpl = document.createElement("template");
+nestedTpl.innerHTML = `
+  <style>${sheet}</style>
+  <span>Nested</span>
+  <raw-simple></raw-simple>
+`;
 
 class NestedRaw extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = this.template;
+    this.attachShadow(config);
+    this.shadowRoot.appendChild(nestedTpl.content.cloneNode(true));
   }
 
   get template() {
-    return `<style>${sheet}</style>
-              <span>Nested</span>
-              <raw-simple></raw-simple>`;
+    return `
+      <style>${sheet}</style>
+      <span>Nested</span>
+      <raw-simple></raw-simple>`;
   }
 }
 
